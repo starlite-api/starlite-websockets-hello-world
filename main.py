a25286a07ec026b003973802cac9444f89c269e3
Jabbey92 <92jbach@gmail.com>
2022-07-02 15:38:02 -0500

    :sparkles: Adding Websocket Support

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from typing import Any
-from starlite import Starlite, get
+from starlette import websockets as StarletteWebsockets
+from starlite import Starlite, get, WebSocket, websocket
 
 
 @get("/")
 def hello_world() -> dict[str, Any]:
     return {"hello": "world"}
+
+
+@websocket(path="/echo")
+async def echo_websocket_handler(socket: WebSocket) -> None:
+    await socket.accept()
+
+    while True:
+        try:
+            data = await socket.receive_json()
+        except StarletteWebsockets.WebSocketDisconnect:
+            print(f"Websocket {socket.user} Disconnected")
+            break
+        await socket.send_json(data)
+
+    await socket.close()
 
 
 app = Starlite(route_handlers=[hello_world])
